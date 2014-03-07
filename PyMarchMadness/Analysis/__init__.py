a0cf46f827b5c8f4accc4858d3ab6a821f3cbbdb
davidcasterton <david.casterton@gmail.com>
2014-03-06 23:35:44 -0800

    move Main.py into __init__.py

--- PyMarchMadness/Analysis/__init__.py
+++ PyMarchMadness/Analysis/__init__.py
@@ -11,17 +11,25 @@ analysis_dir = os.path.dirname(__file__)
 if analysis_dir not in sys.path:
     sys.path.insert(0, analysis_dir)
 
-available = []
-
+# import all files in Analysis/
+object_list = []  # list of analysis objects
 for file_name in os.listdir(analysis_dir):
     class_name, extension = file_name.split(".")
 
     # skip __init__ and Parent files
-    if extension != "py" or class_name == "__init__" or class_name == "Analysis":
+    if extension != "py" or class_name == "__init__":
         continue
 
+    # import file
     exec("from %s import %s" % (class_name, class_name))
     _object = globals()[class_name]
-    available.append(_object())
+    object_list.append(_object())
 
-num_available = len(available)
+
+def get_objects():
+    """
+    :function: Get list of Analysis objects.
+    :returns: list of Analysis objects
+    :rtype: list
+    """
+    return object_list
--- PyMarchMadness/__init__.py
+++ PyMarchMadness/__init__.py
@@ -1,14 +1,51 @@
+#!/usr/bin/env python
+""":module: Predict outcome of NCAA March Madness tournaments. Load data, analyze, and output results."""
 import os
+import pdb
 import sys
-
-dirname = os.path.dirname(__file__)
-if dirname not in sys.path:
-    sys.path.insert(0, dirname)
 
 import Analysis
 import Constants
-import Main
 import Season
-import Team
 
+__author__ = "David Casterton"
+__license__ = "GPL"
 __version__ = Constants.VERSION
+
+
+# add base directory to python path
+base_dir = os.path.dirname(__file__)
+sys.path.insert(0, base_dir)
+
+if __name__ == "__main__":
+    seasons = {}
+
+    # prompt user to select analysis method
+    prompt = "Please enter number of desired analysis method:\n"
+    analysis_objects = Analysis.get_objects()
+    num_analysis = len(analysis_objects)
+    for i in range(num_analysis):
+        prompt += "%s) %s\n" % (i, analysis_objects[i].get_name())
+    user_input = raw_input(prompt)
+    if user_input.isdigit() and (int(user_input) in range(num_analysis)):
+        analysis_method = analysis_objects[int(user_input)]
+    else:
+        raise Exception("Invalid entry '%s', please enter one of following values %s." % (user_input, range(num_analysis)))
+
+    # analyze each available season
+    for _, row in Constants.KAGGLE_INPUT['seasons'].iterrows():
+        print("Analyzing %s" % row['years'])
+        year = int(row['years'].split("-")[1])
+
+        # create Season object
+        seasons[year] = Season.Season(id=row['season'], years=row['years'], day_zero=row['dayzero'])
+        seasons[year].set_regions(region_w=row['regionW'], region_x=row['regionX'], region_y=row['regionY'], region_z=row['regionZ'])
+
+        # build Teams and run analysis
+        seasons[year].build_teams()
+        seasons[year].generate_matchup_probabilities(analysis_method)
+        seasons[year].generate_bracket(analysis_method)
+
+    # write output to files
+    analysis_method.write_bracket_file(seasons)
+    analysis_method.write_matchup_probabilities_file(seasons)
