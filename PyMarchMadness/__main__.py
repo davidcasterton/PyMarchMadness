0d2917f48b86084765cc9db5e0a1756fc9d34aa6
davidcasterton <david.casterton@gmail.com>
2014-03-19 18:58:11 -0700

    add prompt for Kaggle output

--- PyMarchMadness/__main__.py
+++ PyMarchMadness/__main__.py
@@ -25,12 +25,29 @@ if __name__ == "__main__":
     for i in range(num_analysis_options):
         prompt += "%s) %s\n" % (i, analysis_options[i].get_name())
     user_input = raw_input(prompt)
-
     # check user input
     if user_input.isdigit() and (int(user_input) in range(num_analysis_options)):
         analysis = analysis_options[int(user_input)]
     else:
         raise Exception("Invalid entry '%s', please enter one of following values %s." % (user_input, range(num_analysis_options)))
+
+    # prompt user for Kaggle output
+    kaggle_output_options = {
+        0: "No Kaggle output",
+        1: "Pre-tournament output (5 seasons)",
+        2: "Tournament output (current season)"
+    }
+    num_kaggle_output_options = len(kaggle_output_options)
+    prompt = "Please enter the number corresponding to the desired Kaggle output:\n"
+    for i in range(len(kaggle_output_options)):
+        prompt += "%s) %s\n" % (i, kaggle_output_options[i])
+    user_input = raw_input(prompt)
+    # check user input
+    if user_input.isdigit() and (int(user_input) in range(num_kaggle_output_options)):
+        kaggle_output = kaggle_output_options[int(user_input)]
+    else:
+        raise Exception("Invalid entry '%s', please enter one of following values %s." % (user_input, range(num_kaggle_output_options)))
+
 
     # create TeamFactory
     team_factory = Basketball.TeamFactory()
@@ -51,21 +68,37 @@ if __name__ == "__main__":
 
             tournaments[row.season] = tournament
 
+
+
     # if necessary make output directory
     if not os.path.isdir(Constants.OUTPUT_DIR):
         os.mkdir(Constants.OUTPUT_DIR)
 
-    # aggregate matchup probability DataFrames from multiple seasons
-    matchup_probabilities = pandas.DataFrame()
-    for year in range(Constants.CURRENT_YEAR-5, Constants.CURRENT_YEAR):
-        season_id = Constants.SEASON_YEAR_TO_ID[year]
+    if kaggle_output == kaggle_output_options[1]:
+        # aggregate matchup probability DataFrames from multiple seasons
+        matchup_probabilities = pandas.DataFrame()
+        for year in range(Constants.CURRENT_YEAR-5, Constants.CURRENT_YEAR):
+            season_id = Constants.SEASON_YEAR_TO_ID[year]
+            tournament = tournaments.get(season_id)
+            if tournament:
+                matchup_probabilities = matchup_probabilities.append(tournament.get_matchup_probabilities())
+        # write matchup probabilities file
+        file_path = os.path.join(Constants.OUTPUT_DIR, "%s-matchup_probabilities.csv" % analysis.get_name(remove_spaces=True))
+        matchup_probabilities.to_csv(file_path, mode="w", index=False)
+        print("Wrote match up probabilities to: '%s'" % file_path)
+    elif kaggle_output == kaggle_output_options[2]:
+        # aggregate matchup probability DataFrames from multiple seasons
+        matchup_probabilities = pandas.DataFrame()
+        season_id = Constants.SEASON_YEAR_TO_ID[Constants.CURRENT_YEAR]
         tournament = tournaments.get(season_id)
         if tournament:
             matchup_probabilities = matchup_probabilities.append(tournament.get_matchup_probabilities())
-    # write matchup probabilities file
-    file_path = os.path.join(Constants.OUTPUT_DIR, "%s-matchup_probabilities.csv" % analysis.get_name(remove_spaces=True))
-    matchup_probabilities.to_csv(file_path, mode="w", index=False)
-    print("Wrote match up probabilities to: '%s'" % file_path)
+        # write matchup probabilities file
+        file_path = os.path.join(Constants.OUTPUT_DIR, "%s-matchup_probabilities.csv" % analysis.get_name(remove_spaces=True))
+        matchup_probabilities.to_csv(file_path, mode="w", index=False)
+        print("Wrote match up probabilities to: '%s'" % file_path)
+    else:
+        print("Not writing match up probabilites file")
 
     # write bracket file
     file_path = os.path.join(Constants.OUTPUT_DIR, "%s-tournament_brackets.txt" % analysis.get_name(remove_spaces=True))
